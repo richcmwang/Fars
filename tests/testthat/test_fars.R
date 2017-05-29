bda414a415ad58fe4b3487938e5d7d35fa22e2fe
Richard Wang <richcmwang@gmail.com>
2017-05-29 21:04:00 +0100

    add 1 example which needs to access data

--- tests/testthat/test_fars.R
+++ tests/testthat/test_fars.R
@@ -2,7 +2,7 @@ context("test Fars functions")
 
 test_that("test Fars functions",  {
   expect_that(make_filename(2014), is_a("character"))
-  # expect_that(fars_read(make_filename(2014)), is_a("data.frame"))
+  expect_that(fars_read(make_filename(2014)), is_a("data.frame"))
   # expect_that(fars_read_years(c(2018)), gives_warning())
   # expect_that(fars_summarize_years(c(2018)), throws_error())
   # expect_that(fars_summarize_years(c(2014)), is_a("tbl_df"))
