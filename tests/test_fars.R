319e5f9310bd9f38abcc81ae6a2b666934c7a00b
Richard Wang <richcmwang@gmail.com>
2017-05-29 14:33:57 +0100

    run devtools:testthat() run devtools:load_all() run use_package(...)

--- tests/test_fars.R
+++
@@ -1,5 +0,0 @@
-# This is a test file
-expect_that(make_filename(2014), is_a("character"))
-expect_that(fars_read(make_filename(2014)), is_a("data.frame"))
-expect_that(fars_read_years(c(2018)), gives_warning())
-expect_that(fars_summarize_years(c(2018)), throws_error())
