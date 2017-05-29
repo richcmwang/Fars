42f465cde9342b8f32d3c72f000c84d2e4ed52ca
Richard Wang <richcmwang@gmail.com>
2017-05-29 14:34:32 +0100

    testthat file

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,10 @@ library(testthat)
 library(Fars)
 
 test_check("Fars")
+
+# This is a test file
+expect_that(make_filename(2014), is_a("character"))
+expect_that(fars_read(make_filename(2014)), is_a("data.frame"))
+expect_that(fars_read_years(c(2018)), gives_warning())
+expect_that(fars_summarize_years(c(2018)), throws_error())
+
